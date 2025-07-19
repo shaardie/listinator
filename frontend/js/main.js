ee753ed868cdc5ffe43285d023f0847c84401fa5
Sven Haardiek <sven.haardiek@uni-muenster.de>
2025-07-19 23:06:22 +0200

    update

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,95 +1,110 @@
+// updateEntryinDOM removes a potential existing entry and creates a new one in the DOM
+function updateEntryInDOM(entry) {
+  const li = entry.Bought ? createBoughtEntry(entry) : createBuyEntry(entry);
+  const list = entry.Bought
+    ? document.getElementById("boughtList")
+    : document.getElementById("buyList");
+  document.getElementById(entry.ID)?.remove();
+  list.prepend(li);
+}
+
+function updateEntry(entry) {
+  fetch(`/api/v1/entries/${entry.ID}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      Name: entry.Name,
+      Number: entry.Number,
+      Bought: entry.Bought,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`failed to sync with server, ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => updateEntryInDOM(json))
+    .catch((error) => {
+      console.log(error);
+      alert("error reaching server, best way to fix this is to reload");
+    });
+}
+
 function createBoughtEntry(entry) {
   const li = document.createElement("li");
   li.id = entry.ID;
   li.textContent = entry.Name;
 
-  const divRight = document.createElement("div");
-  divRight.classList.add("entryAttributes");
+  const div = document.createElement("div");
+  div.classList.add("entryAttributes");
 
   const button = document.createElement("button");
   button.textContent = "Add";
-  button.addEventListener("click", () => {
-    fetch(`/api/v1/entries/${entry.ID}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        Name: entry.Name,
-        Number: 1,
-        Bought: false,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`failed to sync with server, ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((json) => {
-        document.getElementById(json.ID)?.remove();
-        const buyList = document.getElementById("buyList");
-        buyList.prepend(createBuyEntry(json));
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("error reaching server, best way to fix this is to reload");
-      });
-  });
-  divRight.appendChild(button);
+  button.addEventListener("click", () =>
+    updateEntry({
+      ID: entry.ID,
+      Name: entry.Name,
+      Bought: false,
+      Number: 1,
+    }),
+  );
+  div.appendChild(button);
 
-  li.appendChild(divRight);
+  li.appendChild(div);
   return li;
 }
+
 function createBuyEntry(entry) {
   const li = document.createElement("li");
   li.id = entry.ID;
   li.textContent = entry.Name;
 
-  const divRight = document.createElement("div");
-  divRight.classList.add("entryAttributes");
+  const div = document.createElement("div");
+  div.classList.add("entryAttributes");
 
   const input = document.createElement("input");
   input.value = entry.Number;
   input.type = "text";
-  divRight.appendChild(input);
+  input.addEventListener("change", (event) => {
+    const li = event.currentTarget;
+    const number = Number(li.value);
+    // Reset on NaN
+    if (isNaN(number) || li.value === "") {
+      li.value = entry.Number;
+      return;
+    }
+    if (entry.Number === number) {
+      return;
+    }
+    updateEntry({
+      ID: entry.ID,
+      Name: entry.Name,
+      Bought: entry.Bought,
+      Number: number,
+    });
+  });
+  div.appendChild(input);
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.addEventListener("click", () => {
-    fetch(`/api/v1/entries/${entry.ID}`, {
-      method: "PUT",
-      body: JSON.stringify({
-        Name: entry.Name,
-        Number: entry.Number,
-        Bought: true,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`failed to sync with server, ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((json) => {
-        document.getElementById(json.ID)?.remove();
-        const boughtList = document.getElementById("boughtList");
-        boughtList.prepend(createBoughtEntry(json));
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("error reaching server, best way to fix this is to reload");
-      });
-  });
-  divRight.appendChild(checkbox);
+  checkbox.addEventListener("click", () =>
+    updateEntry({
+      ID: entry.ID,
+      Name: entry.Name,
+      Bought: true,
+      Number: 1,
+    }),
+  );
+  div.appendChild(checkbox);
 
-  li.appendChild(divRight);
+  li.appendChild(div);
   return li;
 }
 
+// Reset filtering the entries in the DOM
 function resetFilterEntries() {
   const buyList = document.getElementById("buyList");
   const boughtList = document.getElementById("boughtList");
@@ -101,6 +116,7 @@ function resetFilterEntries() {
   });
 }
 
+// Filter the entries in the DOM
 function filterEntries(filter) {
   filter = filter.toLowerCase();
   const buyList = document.getElementById("buyList");
@@ -121,8 +137,10 @@ function filterEntries(filter) {
   });
 }
 
+function setEntryNumber(entry, li) {}
+
 document.addEventListener("DOMContentLoaded", async () => {
-  // render list on start
+  // Get entries from server on start
   fetch("/api/v1/entries")
     .then((response) => {
       if (!response.ok) {
@@ -131,17 +149,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       return response.json();
     })
     .then((json) => {
-      const buyList = document.getElementById("buyList");
-      const boughtList = document.getElementById("boughtList");
-      buyList.innerHTML = "";
-      boughtList.innerText = "";
-      json.forEach((entry) => {
-        if (entry.Bought) {
-          boughtList.append(createBoughtEntry(entry));
-        } else {
-          buyList.append(createBuyEntry(entry));
-        }
-      });
+      json.forEach((entry) => updateEntryInDOM(entry));
     })
     .catch((error) => {
       console.log(error);
@@ -185,10 +193,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
         return response.json();
       })
-      .then((json) => {
-        const list = document.getElementById("buyList");
-        list.prepend(createBuyEntry(json));
-      })
+      .then((json) => updateEntryInDOM(json))
       .catch((error) => {
         console.log(error);
         alert("error reaching server, best way to fix this is to reload");
