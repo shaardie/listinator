b79784afdcd55c576e7236324435d7770d7f0c54
Sven Haardiek <sven.haardiek@uni-muenster.de>
2025-07-22 15:17:17 +0200

    put all DOM-depended code in the DOMContentLoaded-Hander

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -21,156 +21,157 @@ async function apiFetch(url, options = {}) {
   return response.json();
 }
 
-// updateEntryinDOM removes a potential existing entry and creates a new one in the DOM
-function updateEntryInDOM(entry) {
-  const li = createEntry(entry);
-  const list = entry.Bought
-    ? document.getElementById("boughtList")
-    : document.getElementById("buyList");
-  document.getElementById(entry.ID)?.remove();
-  list.prepend(li);
-}
-
-// updateEntry updates an entry on the backend and trigger an update in the DOM
-function updateEntry(entry) {
-  apiFetch(`/api/v1/entries/${entry.ID}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      Name: entry.Name,
-      Number: entry.Number,
-      Bought: entry.Bought,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((json) => updateEntryInDOM(json))
-    .catch((error) => showError(error));
-}
-
-// createEntry creates a new li element with the entry in it
-function createEntry(entry) {
-  const li = document.createElement("li");
-  li.id = entry.ID;
-  li.textContent = entry.Name;
-
-  const div = document.createElement("div");
-  div.classList.add("entryAttributes");
-
-  if (entry.Bought === true) {
-    // Bought entry
-    const button = document.createElement("button");
-    button.textContent = "+";
-    button.addEventListener("click", () =>
-      updateEntry({
-        ID: entry.ID,
-        Name: entry.Name,
-        Bought: false,
-        Number: "",
-      }),
-    );
-    div.appendChild(button);
-  } else {
-    // Buy entry
-    const input = document.createElement("input");
-    input.value = entry.Number;
-    input.type = "text";
-    input.addEventListener("change", (event) => {
-      const li = event.currentTarget;
-      const number = li.value;
-      if (entry.Number === number) {
-        return;
-      }
-      updateEntry({
-        ID: entry.ID,
-        Name: entry.Name,
-        Bought: entry.Bought,
-        Number: number,
-      });
-    });
-    div.appendChild(input);
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.addEventListener("click", () =>
-      updateEntry({
-        ID: entry.ID,
-        Name: entry.Name,
-        Bought: true,
-        Number: "",
-      }),
-    );
-    div.appendChild(checkbox);
-  }
-  li.appendChild(div);
-  return li;
-}
-
-// resetFilterEntries reset filtering the entries in the DOM
-function resetFilterEntries() {
-  const buyList = document.getElementById("buyList");
-  const boughtList = document.getElementById("boughtList");
-  buyList.childNodes.forEach((li) => {
-    li.classList.remove("invisible");
-  });
-  boughtList.childNodes.forEach((li) => {
-    li.classList.remove("invisible");
-  });
-}
-
-// filterEntries filters the entries in the DOM
-function filterEntries(filter) {
-  filter = filter.toLowerCase();
-  const buyList = document.getElementById("buyList");
-  const boughtList = document.getElementById("boughtList");
-  buyList.childNodes.forEach((li) => {
-    if (li.textContent.toLowerCase().includes(filter)) {
-      li.classList.remove("invisible");
-    } else {
-      li.classList.add("invisible");
-    }
-  });
-  boughtList.childNodes.forEach((li) => {
-    if (li.textContent.toLowerCase().includes(filter)) {
-      li.classList.remove("invisible");
-    } else {
-      li.classList.add("invisible");
-    }
-  });
-}
-
 document.addEventListener("DOMContentLoaded", async () => {
-  // Get entries from server on start
-  apiFetch("/api/v1/entries")
-    .then((json) => {
-      json.forEach((entry) => updateEntryInDOM(entry));
-    })
-    .catch((error) => showError(error));
+  // Dom Element
+  const buyList = document.getElementById("buyList");
+  const boughtList = document.getElementById("boughtList");
+  const searchInput = document.getElementById("searchInput");
+  const addButton = document.getElementById("addButton");
 
-  // on Enter in searchInput trigger button
-  document
-    .getElementById("searchInput")
-    .addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("addButton").click();
+  /**
+   * updateEntryInDOM updates the entry in the DOM.
+   * @param {Object} entry - The entry object to update.
+   */
+  function updateEntryInDOM(entry) {
+    const li = createEntry(entry);
+    const list = entry.Bought ? boughtList : buyList;
+    document.getElementById(entry.ID)?.remove();
+    list.prepend(li);
+  }
+
+  /**
+   * updateEntry updates an entry on the server.
+   * @param {Object} entry - The entry object to update.
+   * @returns {Promise<void>} - A promise that resolves when the entry is updated.
+   */
+  function updateEntry(entry) {
+    apiFetch(`/api/v1/entries/${entry.ID}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        Name: entry.Name,
+        Number: entry.Number,
+        Bought: entry.Bought,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((json) => updateEntryInDOM(json))
+      .catch((error) => showError(error));
+  }
+
+  /**
+   * createEntry creates a list item for an entry.
+   * @param {Object} entry - The entry object to create a list item for.
+   * @returns {HTMLLIElement} - The created list item element.
+   */
+  function createEntry(entry) {
+    const li = document.createElement("li");
+    li.id = entry.ID;
+    li.textContent = entry.Name;
+
+    const div = document.createElement("div");
+    div.classList.add("entryAttributes");
+
+    if (entry.Bought === true) {
+      // Bought entry
+      const button = document.createElement("button");
+      button.textContent = "+";
+      button.addEventListener("click", () =>
+        updateEntry({
+          ID: entry.ID,
+          Name: entry.Name,
+          Bought: false,
+          Number: "",
+        }),
+      );
+      div.appendChild(button);
+    } else {
+      // Buy entry
+      const input = document.createElement("input");
+      input.value = entry.Number;
+      input.type = "text";
+      input.addEventListener("change", (event) => {
+        const li = event.currentTarget;
+        const number = li.value;
+        if (entry.Number === number) {
+          return;
+        }
+        updateEntry({
+          ID: entry.ID,
+          Name: entry.Name,
+          Bought: entry.Bought,
+          Number: number,
+        });
+      });
+      div.appendChild(input);
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("click", () =>
+        updateEntry({
+          ID: entry.ID,
+          Name: entry.Name,
+          Bought: true,
+          Number: "",
+        }),
+      );
+      div.appendChild(checkbox);
+    }
+    li.appendChild(div);
+    return li;
+  }
+
+  /**
+   * resetFilterEntries resets the visibility of all entries in the buy and bought lists.
+   * It makes all entries visible again.
+   * This is useful when the user clears the search input.
+   * @returns {void}
+   */
+  function resetFilterEntries() {
+    buyList.childNodes.forEach((li) => {
+      li.classList.remove("invisible");
+    });
+    boughtList.childNodes.forEach((li) => {
+      li.classList.remove("invisible");
+    });
+  }
+
+  /**
+   * filterEntries filters the entries in the buy and bought lists based on the search input.
+   * It hides entries that do not match the search input.
+   * @returns {void}
+   */
+  function filterEntries() {
+    const filter = searchInput.value.toLowerCase();
+    buyList.childNodes.forEach((li) => {
+      if (li.textContent.toLowerCase().includes(filter)) {
+        li.classList.remove("invisible");
+      } else {
+        li.classList.add("invisible");
       }
     });
+    boughtList.childNodes.forEach((li) => {
+      if (li.textContent.toLowerCase().includes(filter)) {
+        li.classList.remove("invisible");
+      } else {
+        li.classList.add("invisible");
+      }
+    });
+  }
 
-  // trigger re-rendering of the list on search input changes
-  document.getElementById("searchInput").addEventListener("keyup", () => {
-    filterEntries(document.getElementById("searchInput")?.value);
-  });
-
-  // add entry on add button push
-  document.getElementById("addButton").addEventListener("click", () => {
-    const input = document.getElementById("searchInput");
-    const name = input.value;
+  /**
+   * addButtonClick handles the click event on the add button.
+   * It retrieves the value from the search input, ignores empty input,
+   * and sends a POST request to add a new entry.
+   */
+  function addButtonClick() {
+    const name = searchInput.value;
     // ignore empty input
     if (name === "") {
       return;
     }
-    input.value = "";
+    searchInput.value = "";
     resetFilterEntries();
     apiFetch("/api/v1/entries", {
       method: "POST",
@@ -184,5 +185,32 @@ document.addEventListener("DOMContentLoaded", async () => {
     })
       .then((json) => updateEntryInDOM(json))
       .catch((error) => showError(error));
-  });
+  }
+
+  /**
+   * searchInputKeydown handles the keydown event on the search input and trigger a click on the add button.
+   * This allows the user to add an entry by pressing the Enter key.
+   * @param {KeyboardEvent} event - The keydown event on the search input.
+   * @returns {void}
+   */
+  function searchInputKeydown(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addButton.click();
+    }
+  }
+
+  // on Enter in searchInput trigger button
+  searchInput.addEventListener("keydown", searchInputKeydown);
+  // trigger re-rendering of the list on search input changes
+  searchInput.addEventListener("keyup", filterEntries);
+  // add entry on add button push
+  addButton.addEventListener("click", addButtonClick);
+
+  // Get entries from server on start
+  apiFetch("/api/v1/entries")
+    .then((json) => {
+      json.forEach((entry) => updateEntryInDOM(entry));
+    })
+    .catch((error) => showError(error));
 });
